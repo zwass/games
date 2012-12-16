8bd3fea53bdf987ac1a326311d92ae864ffba5ae
Zachary Wasserman <zachwass2000@gmail.com>
2012-12-15 20:13:46 -0500

    Initial

---
+++ OneTwoTen.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# OPTIONS -Wall #-}
+
+module OneTwoTen where
+
+import Solver
+
+data OTTBoard = OTTBoard {turn :: Player,
+                          ottBoard :: Integer}
+                deriving (Show, Read, Eq, Ord)
+
+boardSize :: Integer
+boardSize = 10
+
+instance SolvableGame OTTBoard where
+  initialPosition = OTTBoard PlayerOne 0
+  doMove b m =
+    OTTBoard (nextPlayer . turn $ b) (m + (ottBoard b))
+  primitive = ottPrimitive
+  generateMoves _ = [1, 2]
+  whoseTurn = turn
+
+ottPrimitive :: OTTBoard -> Value
+ottPrimitive b
+  | ottBoard b < boardSize = Undecided
+  | turn b == PlayerOne = Lose
+  | otherwise = Win
---
+++ Solver.hs
@@ -0,0 +1,81 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Solver where
+
+import qualified Data.Map as M
+import Data.Map (Map)
+import Control.Monad.State.Lazy (State)
+import qualified Control.Monad.State as S
+
+import Debug.Trace
+
+--For storing the value of a move/state
+--Win denotes a win for player one
+--Lose denotes a win for player two
+data Value = Undecided | Lose | Tie | Win
+           deriving (Show, Read, Eq, Ord)
+
+--For storing whose turn it is
+data Player = PlayerOne | PlayerTwo
+            deriving (Show, Read, Eq, Ord)
+
+nextPlayer :: Player -> Player
+nextPlayer PlayerOne = PlayerTwo
+nextPlayer PlayerTwo = PlayerOne
+
+data GameState a = GameState {
+  gs :: a -- Stores state of the game. Should allow implementing game to decide
+          -- whose turn it is.
+  }
+
+type Move = Integer
+
+class (Ord a, Show a) => SolvableGame a where -- Parametrized by game state
+  initialPosition :: a
+  doMove :: a -> Move -> a
+  primitive :: a -> Value
+  generateMoves :: a -> [Move]
+  whoseTurn :: a -> Player
+
+type GameTree a = Map a Value
+
+type SolverState a = State (GameTree a) Value
+
+playerOneMax :: [Value] -> Value
+playerOneMax vals | null vals = error "No children from non-Leaf node"
+                  | otherwise = maximum vals
+
+playerTwoMax :: [Value] -> Value
+playerTwoMax vals | null vals = error "No children from non-Leaf node"
+                  | otherwise = minimum . filter (/= Undecided) $ vals
+
+solveGame :: SolvableGame a => GameTree a
+solveGame = S.execState (solve initialPosition) M.empty where
+  solve :: SolvableGame a => a -> SolverState a
+  solve pos = do
+    s <- S.get
+    -- traceShow (M.size s) (return ())
+    -- Try to use memoized value
+    case M.lookup pos s of
+      -- Just use memoized value
+      Just val -> return val
+      -- Compute value
+      Nothing -> case primitive pos of
+        -- At an internal node. Recursively solve for value.
+        Undecided -> do
+          let children = map (doMove pos) (generateMoves pos)
+              turn = whoseTurn pos
+              maxFun = if turn == PlayerOne then playerOneMax else playerTwoMax
+          childVals <- sequence $ map solve children
+          let val = maxFun childVals
+          -- Memoize the computed value
+          S.modify (M.insert pos val) -- Memoize
+          return val
+        -- At a leaf
+        val -> do
+          S.modify (M.insert pos val) -- Memoize the leaf value
+          return val -- Leaf
+
+getValue :: SolvableGame a => a -> Value
+getValue x = M.findWithDefault (error $ "No value for " ++ show x) x solveGame
+            
