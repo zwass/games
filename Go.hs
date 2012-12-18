5795f367b75af16ca49d31db52a8b6daff635783
Ceasar Bautista <cbautista2010@gmail.com>
2012-12-17 20:35:42 -0500

    refactor some

--- Go.hs
+++ Go.hs
@@ -26,24 +26,16 @@ data GoGame = GoGame {turn :: Player, position :: Position, history :: [Move]}
     deriving (Eq, Ord)
 
 -- A point on the board where a horizontal line meets a vertical line is called an intersection.
+
 newtype Intersection = I (Int, Int) deriving (Eq, Ord, Show)
 
-intersections :: GoGame -> [Intersection]
-intersections = M.keys . position
-
-
-boardSize :: GoGame -> Int
-boardSize = undefined
-
-propIntersections :: GoGame -> Bool
-propIntersections b = length (intersections b) == boardSize b * boardSize b
-
 -- Two intersections are said to be adjacent if they are connected by a horizontal or vertical line with no other position between them.
-adjacents :: Intersection -> Set Intersection
-adjacents (I (x, y)) = S.fromList $ P.map I [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 isAdjacent :: Intersection -> Intersection -> Bool
 isAdjacent a b = S.member a $ adjacents b
+
+adjacents :: Intersection -> Set Intersection
+adjacents (I (x, y)) = S.fromList $ P.map I [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 -- Go is played with playing tokens known as stones.
 -- At any time in the game, each intersection on the board is in one and only one of the following three states:
@@ -52,6 +44,15 @@ isAdjacent a b = S.member a $ adjacents b
 --  3) occupied by a white stone. 
 data IState = Empty | Stone Color deriving (Show, Eq, Ord)
 
+isEmpty :: IState -> Bool
+isEmpty = (== Empty)
+
+stoneColor :: IState -> Color
+stoneColor (Stone c) = c
+stoneColor _ = error "invalid color"
+
+-- A position consists of an indication of the state of each intersection.
+type Position = Map Intersection IState
 
 state :: Position -> Intersection -> Maybe IState
 state p i = M.lookup i p
@@ -59,40 +60,35 @@ state p i = M.lookup i p
 knownState :: Position -> Intersection -> IState
 knownState p i = fromJust $ state p i
 
-isEmpty :: IState -> Bool
-isEmpty = (== Empty)
-
--- A position consists of an indication of the state of each intersection.
-type Position = Map Intersection IState
-
--- List of empty intersections
+-- List of empty Intersectionss
 empties :: GoGame -> [Intersection]
 empties = M.keys . M.filter isEmpty . position
 
--- List of occupied intersection
+-- List of occupied Intersections
 stones :: Position -> Position
 stones = M.filter (not . isEmpty)
 
-iColor :: IState -> Color
-iColor (Stone c) = c
-iColor _ = error "invalid color"
-
+-- Map of Intersection occupied a stone owned by Player
 pieces :: Player -> Position -> Position
-pieces pl = M.filter ((== pColor) . iColor) . stones where pColor = playerColor pl
+pieces pl = M.filter ((== pColor) . stoneColor) . stones where pColor = playerColor pl
 
-onGoGame :: Position -> Intersection -> Bool
-onGoGame p i = isJust $ state p i
+-- Check if an Intersection is on the board
+onBoard :: Position -> Intersection -> Bool
+onBoard p i = isJust $ state p i
 
+-- Get valid adjacent Intersections
 safeAdjs :: Position -> Intersection -> Set Intersection
-safeAdjs p = S.filter (onGoGame p) . adjacents
+safeAdjs p = S.filter (onBoard p) . adjacents
 
 -- In a given position, two stones of the same color (or two empty intersections) are said to be connected if it is possible to pass from one to the other by a succession of stones of that color (or empty intersections, respectively) in which any two consecutive ones are adjacent.
 connected :: Intersection -> Intersection -> Position -> Bool
 connected a b p = S.member a $ connections b p
 
+-- Check if two intersections are occupied by the same colored piece
 friendly :: Position -> Intersection -> Intersection -> Bool
 friendly p a b = f a == f b where f = knownState p
 
+-- List all the Intersections that piece at the given Intersection is connected to
 connections :: Intersection -> Position -> Set Intersection
 connections i p = findFixedPoint (S.singleton i) (S.filter (friendly p i) . safeAdjs p)
     where
@@ -115,14 +111,6 @@ liberties i p = S.unions $ S.toList $ S.map emptyAdjs $ connections i p
 
 propLiberties :: Intersection -> Intersection -> Position -> Property
 propLiberties a b p = connected a b p ==> liberties a p == liberties b p
-
-
--- Turn a move into an intersection
-moveToIntersection :: Move -> GoGame -> Intersection
-moveToIntersection m = snd . fromJust . find ((== m) . fst) . zip [1..] . M.keys . position
-
-intersectionToMove :: Intersection -> GoGame -> Move
-intersectionToMove i = fst . fromJust . find ((== i) . snd) . zip [1..] . M.keys . position
 
 
 -- At the beginning of the game, the board is empty.
@@ -154,17 +142,9 @@ goGenerateMoves b = pass : plays b
 propGenMoves :: GoGame -> Bool
 propGenMoves b = length (goGenerateMoves b) == length (empties b) + 1
 
-
+-- Check if the piece at the given intersection has any liberties
 hasLiberty :: Intersection -> Position -> Bool
 hasLiberty i p = S.size (liberties i p) > 0
-
--- Get the opposing stones
-plStones :: Position -> Player -> Position
-plStones p pl = M.filter f p
-    where
-        f s = case s of
-                Empty -> False
-                Stone c -> c == playerColor pl
 
 -- A play consists of the following steps (performed in the prescribed order):
 --  (Playing a stone) Placing a stone of their color on an empty intersection (chosen subject to Rule 8).
@@ -178,9 +158,11 @@ play m b = doCapture pl $ doCapture op $ playStone (moveToIntersection m b) pl p
     pl = turn b
     op = nextPlayer pl
 
+-- Play a stone owned by the given Player at the given Intersection
 playStone :: Intersection -> Player -> Position -> Position
 playStone i pl = M.insert i (Stone $ playerColor pl)
 
+-- Remove all pieces owned by the given Player that do not have any liberties
 doCapture :: Player -> Position -> Position
 doCapture pl p = M.mapWithKey f p
     where
@@ -206,6 +188,14 @@ evalPosition p
 
 goWhoseTurn :: GoGame -> Player
 goWhoseTurn = turn
+
+-- Convert a Move into an Intersection
+moveToIntersection :: Move -> GoGame -> Intersection
+moveToIntersection m = snd . fromJust . find ((== m) . fst) . zip [1..] . M.keys . position
+
+-- Convert a Intersection into an Intersection
+intersectionToMove :: Intersection -> GoGame -> Move
+intersectionToMove i = fst . fromJust . find ((== i) . snd) . zip [1..] . M.keys . position
 
 instance SolvableGame GoGame where
   initialPosition = goInitialPosition 2
@@ -240,9 +230,6 @@ makeRandMove b = do
     m <- genMove b
     return $ goDoMove b m
 
-test :: GoGame
-test = P.foldl goDoMove (goInitialPosition 3) [1, 2, 3, 4]
-
 instance Arbitrary GoGame where
     arbitrary = do
         b <- makeRandMove (goInitialPosition 5)
@@ -259,35 +246,3 @@ instance Arbitrary Position where
     arbitrary = do
         b <- arbitrary :: Gen GoGame
         return $ position b
-{-
-bfs :: Ord a => Set a -> (a -> [a]) -> (a -> Bool) -> a -> Set a
-bfs visited gen reject s
-    | S.member s visited = S.empty
-    | reject s = S.empty
-    | otherwise = S.insert s $ S.unions $ P.map (bfs (S.insert s visited) gen reject) (gen s)
-
-connections :: Intersection -> Position -> Set Intersection
-connections a p = bfs visited gen reject a
-    where
-        visited = S.empty
-        gen i = S.toList $ safeAdjs p i
-        reject i = state p i /= state p a
--}
-
-
-{-
-connected :: Intersection -> Intersection -> Position -> Bool
-connected a b p = state a p == state b p && bfs visited gen accept reject a
-    where
-        visited = S.empty
-        gen i = S.toList $ safeAdjs i p
-        accept = (== b)
-        reject x = state x p /= state a p
-
-bfs :: Ord a => Set a -> (a -> [a]) -> (a -> Bool) -> (a -> Bool) -> a -> Bool
-bfs visited gen accept reject s
-    | S.member s visited = False
-    | accept s = True
-    | reject s = False
-    | otherwise = any (bfs (S.insert s visited) gen accept reject) (gen s)
--}
